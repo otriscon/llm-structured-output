66ead886f11b964bd586230cdee192709edcc2ce
Oscar Triscon <github@triscon.com>
2024-04-14 17:32:48 -0400

    Correct issue in bias_logits()
    Tokens above the highest accepted one were not being rejected in
    one of the two code paths.

--- src/llm_structured_output/util/bitmap.py
+++ src/llm_structured_output/util/bitmap.py
@@ -16,9 +16,23 @@ def count_set_bits(bitmap: int) -> int:
 
 def highest_bit_set(bitmap: int) -> int:
     """
-    Return the highest bit set in the bitmap.
+    Return the index of the highest bit set in the bitmap.
     """
     return bitmap.bit_length() - 1
+
+
+def bitmap_complement(bitmap: int, set_size: int = None) -> int:
+    """
+    Negate the bits in the bitmap.
+    Since the bitmap is encoded as a Python int, it can be of arbitrary length.
+    I.e. we don't know how many zeros are above the top set bit. The set_size
+    parameter can be passed to indicate the number of bits in the bitmap (which
+    is akin to the number of members in the set it represents). If unspecified,
+    the top set bit in the bitmap is used as its set size. 
+    """
+    if not set_size:
+        set_size = bitmap.bit_length()
+    return (1 << set_size) - 1 - bitmap
 
 
 def enumerate_set_bits(bitmap: int) -> Iterable[int]:
@@ -35,17 +49,26 @@ def enumerate_set_bits(bitmap: int) -> Iterable[int]:
 def bias_logits(np, logits, accepted_token_bitmap):
     """
     Apply a -inf bias to tokens that will not be accepted.
+    Rather than import here, the np parameters is numpy or a compatible library
+    import, such as mlx.core.
     """
-    highest_token = highest_bit_set(accepted_token_bitmap)
-    match_count = count_set_bits(accepted_token_bitmap)
+    vocab_size = logits.shape[0]
+    highest_token_accepted = highest_bit_set(accepted_token_bitmap)
+    accepted_token_count = count_set_bits(accepted_token_bitmap)
     # Check whether there's more tokens to be rejected or to be allowed, then do what's less work.
-    if match_count <= highest_token / 2:
+    if accepted_token_count <= highest_token_accepted / 2:
+        bias = np.full(vocab_size, -inf)
         indices = np.array([*enumerate_set_bits(accepted_token_bitmap)])
-        array = np.full(logits.shape, -inf)
-        array[indices] = 0
+        bias[indices] = 0
     else:
-        rejected_token_bitmap = (1 << (highest_token + 1)) - 1 - accepted_token_bitmap
+        bias = np.concatenate(
+            [
+                np.full(highest_token_accepted + 1, 0),
+                # All tokens above the highest accepted token are rejected.
+                np.full(vocab_size - highest_token_accepted - 1, -inf),
+            ]
+        )
+        rejected_token_bitmap = bitmap_complement(accepted_token_bitmap)
         indices = np.array([*enumerate_set_bits(rejected_token_bitmap)])
-        array = np.full(logits.shape, 0)
-        array[indices] = -inf
-    return np.add(logits, array)
+        bias[indices] = -inf
+    return np.add(logits, bias)
