2a90a31e497462be34dfb79cfccfcb464f132f14
Oscar Triscon <github@triscon.com>
2024-04-20 10:02:21 -0400

    Refactor llm_schema example to separate prompt evaluation.

--- src/examples/llm_schema.py
+++ src/examples/llm_schema.py
@@ -54,53 +54,111 @@ class Model:
         self.tokenizer = HuggingfaceTokenizerHelper(tokenizer)
         self.vocabulary, self.eos_id = self.tokenizer.extract_vocabulary()
 
+    def _run_model(self, inputs: mx.array, cache=None):
+        """
+        This is like the model's __call__() method as implemented in
+        `mlx-examples/llms/mlx_lm/models/*.py`, except it is also able
+        to apply a causal mask once a cache is in place.
+        """
+        model = self.model.model
+        h = model.embed_tokens(inputs)
+
+        mask = None
+        T = h.shape[1]  # pylint: disable=invalid-name
+        if T > 1:
+            if cache is None:
+                S = 0  # pylint: disable=invalid-name
+            else:
+                S = cache[0][0].shape[2]  # pylint: disable=invalid-name
+            mask = nn.MultiHeadAttention.create_additive_causal_mask(S + T)
+            mask = mask.split([S])[1].astype(h.dtype)
+
+        if cache is None:
+            cache = [None] * len(model.layers)
+
+        for e, layer in enumerate(model.layers):
+            h, cache[e] = layer(h, mask, cache[e])
+
+        out = model.norm(h)
+        return self.model.lm_head(out), cache
+
     def _decode(self, tokens):
         return self.tokenizer.no_strip_decode(tokens)
 
-    def sample(self, logits, temp):
+    def _evaluate_prompt(
+        self, prompt: list[int], prior_prompt: list[int] = None, prior_cache=None
+    ):
+        if prior_prompt:
+            i = 0
+            for i, t in enumerate(prior_prompt):
+                if len(prompt) < i or prompt[i] != t:
+                    break
+            cache = [
+                (
+                    layer_key_cache[:, :, :i, :],
+                    layer_value_cache[:, :, :i, :],
+                )
+                for layer_key_cache, layer_value_cache in prior_cache
+            ]
+            tokens = prompt[i:]
+        else:
+            tokens = prompt
+            cache = None
+
+        logits, cache = self._run_model(mx.array(tokens)[None], cache)
+        return logits, cache
+
+    def _debug_top_tokens(self, logits, count=10):
+        token_logits = sorted(
+            enumerate(logits.tolist()), key=itemgetter(1), reverse=True
+        )
+        top_tokens = [
+            (self._decode([t]), p) for t, p in token_logits[:count] if p != -inf
+        ]
+        debug("TOP TOKENS:", top_tokens)
+
+    def _sample(self, logits, temp):
         if temp == 0:
             result = mx.argmax(logits, axis=-1)
         else:
             result = mx.random.categorical(logits * (1 / temp))
         return result.item()
 
-    def generate_without_schema(
-        self, prompt: Iterable[int], temp: Optional[float] = 0.0
-    ):
+    def generate_without_schema(self, logits, cache, temp: Optional[float] = 0.0):
         """
         For testing / comparison purposes.
         """
-        tokens = prompt
-        cache = None
-        while tokens[-1] != self.eos_id:
-            logits, cache = self.model(mx.array(tokens)[None], cache)
-            tokens = [self.sample(logits[0, -1, :], temp)]
+        while True:
+            tokens = [self._sample(logits[0, -1, :], temp)]
             yield tokens
+            if tokens[-1] == self.eos_id:
+                break
+            logits, cache = self.model(mx.array(tokens)[None], cache)
 
     def generate_with_schema(
-        self, prompt: Iterable[int], token_acceptor, temp: Optional[float] = 0.0
+        self, logits, cache, token_acceptor, temp: Optional[float] = 0.0
     ):
-        tokens = prompt
-        cache = None
-        while tokens[-1] != self.eos_id:
-            logits, cache = self.model(mx.array(tokens)[None], cache)
+        while True:
             accepted_token_bitmap = token_acceptor.select_valid_tokens()
             if not accepted_token_bitmap:
                 raise RejectedCompletion()
             logits = bias_logits(mx, logits[0, -1, :], accepted_token_bitmap)
-            tokens = [self.sample(logits, temp)]
+            tokens = [self._sample(logits, temp)]
             yield tokens
             token_acceptor.advance_token(self._decode(tokens))
+            if tokens[-1] == self.eos_id:
+                break
+            logits, cache = self.model(mx.array(tokens)[None], cache)
 
     def generate_with_preemptive_decoding(
         self,
-        prompt: Iterable[int],
+        logits,
+        cache,
         token_acceptor,
         temp: Optional[float] = 0.0,
         max_batch_size=5,
     ):
         """
-        [Experimental]
         Try to generate faster by precomputing two tokens at a time when possible.
         If we know that the acceptor will only accept a small set of tokens after
         the current one, we can evaluate a batch with one entry per possible
@@ -112,11 +170,10 @@ class Model:
         a causal mask), and we can sample those logits to select from which of the
         items in the batch we can select the second token.
         """
-        # Submit prompt for evaluation
-        logits, cache = self._run_model(mx.array(prompt)[None])
+        # Sample token from prompt evaluation
         accepted_token_bitmap = token_acceptor.select_valid_tokens()
         first_token_logits = bias_logits(mx, logits[0, -1, :], accepted_token_bitmap)
-        first_token = self.sample(first_token_logits, temp)
+        first_token = self._sample(first_token_logits, temp)
         tokens = [first_token]
         yield tokens
         token_acceptor.advance_token(self._decode(tokens))
@@ -145,7 +202,7 @@ class Model:
             mx.eval(logits)
 
             first_token_logits = bias_logits(mx, logits[0, 0, :], accepted_token_bitmap)
-            first_token = self.sample(first_token_logits, temp)
+            first_token = self._sample(first_token_logits, temp)
             tokens = [first_token]
 
             if first_token == self.eos_id:
@@ -167,7 +224,7 @@ class Model:
                 second_token_logits = bias_logits(
                     mx, logits[index, 1, :], accepted_token_bitmap
                 )
-                second_token = self.sample(second_token_logits, temp)
+                second_token = self._sample(second_token_logits, temp)
                 tokens.append(second_token)
 
                 token_acceptor.advance_token(self._decode([second_token]))
@@ -186,7 +243,8 @@ class Model:
 
     def generate_with_preemptive_decoding_constant_batch(
         self,
-        prompt: Iterable[int],
+        logits,
+        cache,
         token_acceptor,
         temp: Optional[float] = 0.0,
         max_batch_size=5,
@@ -194,14 +252,13 @@ class Model:
         """
         [Experimental]
         Same as generate_with_preemptive_decoding(), but keeping the batch size
-        constant through the generation to have consistent mathix sizes.
+        constant through the generation to have consistent matrix sizes.
         This is just for experimenting with performance.
         """
-        # Submit prompt for evaluation
-        logits, cache = self._run_model(mx.array([prompt] * max_batch_size))
+        # Sample token from prompt evaluation
         accepted_token_bitmap = token_acceptor.select_valid_tokens()
         first_token_logits = bias_logits(mx, logits[0, -1, :], accepted_token_bitmap)
-        first_token = self.sample(first_token_logits, temp)
+        first_token = self._sample(first_token_logits, temp)
         tokens = [first_token]
         yield tokens
         token_acceptor.advance_token(self._decode(tokens))
@@ -225,7 +282,7 @@ class Model:
             mx.eval(logits)
 
             first_token_logits = bias_logits(mx, logits[0, 0, :], accepted_token_bitmap)
-            first_token = self.sample(first_token_logits, temp)
+            first_token = self._sample(first_token_logits, temp)
             tokens = [first_token]
 
             if first_token == self.eos_id:
@@ -247,7 +304,7 @@ class Model:
                 second_token_logits = bias_logits(
                     mx, logits[index, 1, :], accepted_token_bitmap
                 )
-                second_token = self.sample(second_token_logits, temp)
+                second_token = self._sample(second_token_logits, temp)
                 tokens.append(second_token)
 
                 token_acceptor.advance_token(self._decode([second_token]))
@@ -264,92 +321,22 @@ class Model:
 
             yield tokens
 
-    def _run_model(self, inputs: mx.array, cache=None):
-        """
-        [Experimental]
-        This is like the model's __call__() method as implemented in
-        `mlx-examples/llms/mlx_lm/models/*.py`, except it is able to apply a
-        causal mask once a cache is in place rather than just for the prompt.
-        """
-        model = self.model.model
-        h = model.embed_tokens(inputs)
-
-        mask = None
-        T = h.shape[1]  # pylint: disable=invalid-name
-        if T > 1:
-            if cache is None:
-                S = 0  # pylint: disable=invalid-name
-            else:
-                S = cache[0][0].shape[2]  # pylint: disable=invalid-name
-            mask = nn.MultiHeadAttention.create_additive_causal_mask(S + T)
-            mask = mask.split([S])[1].astype(h.dtype)
-
-        if cache is None:
-            cache = [None] * len(model.layers)
-
-        for e, layer in enumerate(model.layers):
-            h, cache[e] = layer(h, mask, cache[e])
-
-        out = model.norm(h)
-        return self.model.lm_head(out), cache
-
-    def completion(
+    def _generate_tokens(
         self,
-        prompt: Union[str, Iterable[dict[str, str]]],
-        schema: dict,
-        encapsulated: bool = False,
+        generator: Iterable,
         max_tokens: int = 1000,
-        temp: float = 0.0,
-        seed: int = None,
-        preemptive_batch_size: int = 0,
-    ):
-        if seed is not None:
-            mx.random.seed(seed)
-
-        prompt_tokens = self.tokenizer.encode_prompt(prompt)
-
-        if schema:
-            token_acceptor = JsonSchemaAcceptorDriver(
-                schema,
-                self.vocabulary,
-                self.eos_id,
-                is_encapsulated_json=encapsulated,
-            )
-            if preemptive_batch_size > 0:
-                generator = self.generate_with_preemptive_decoding(
-                    prompt_tokens,
-                    token_acceptor,
-                    temp,
-                    max_batch_size=preemptive_batch_size,
-                )
-            else:
-                generator = self.generate_with_schema(
-                    prompt_tokens, token_acceptor, temp
-                )
-        else:
-            generator = self.generate_without_schema(prompt_tokens, temp)
-
+    ) -> Iterable:
         start_time = time.time_ns()
-
         token_count = 0
-        for tokens in generator:
-            if not token_count:
-                yield {
-                    "op": "evaluatedPrompt",
-                    "prompt": prompt,
-                    "token_count": len(prompt_tokens),
-                    "time_ms": (time.time_ns() - start_time) / 1e6,
-                }
-                start_time = time.time_ns()
 
+        for tokens in generator:
             token_count += len(tokens)
 
             try:
                 eos_index = tokens.index(self.eos_id)
+                tokens = tokens[0:eos_index]
             except ValueError:
                 eos_index = -1
-            if eos_index >= 0:
-                tokens = tokens[0:eos_index]
 
             if tokens:
                 text = self._decode(tokens)
@@ -364,7 +351,7 @@ class Model:
                 yield {"op": "stop", "reason": "end"}
                 return
 
-            if token_count == max_tokens:
+            if token_count >= max_tokens:
                 yield {"op": "stop", "reason": "max_tokens"}
                 return
 
@@ -372,14 +359,69 @@ class Model:
 
         assert False
 
-    def _debug_top_tokens(self, logits, count=10):
-        token_logits = sorted(
-            enumerate(logits.tolist()), key=itemgetter(1), reverse=True
-        )
-        top_tokens = [
-            (self._decode([t]), p) for t, p in token_logits[:count] if p != -inf
-        ]
-        debug("TOP TOKENS:", top_tokens)
+    def completion(
+        self,
+        prompt: Union[str, Iterable[dict[str, str]]],
+        schema: dict,
+        encapsulated: bool = False,
+        max_tokens: int = 1000,
+        temp: float = 0.0,
+        seed: int = None,
+        preemptive_batch_size: int = 0,
+    ):
+        if seed is not None:
+            mx.random.seed(seed)
+
+        start_time = time.time_ns()
+        prompt_tokens = self.tokenizer.encode_prompt(prompt)
+        logits, cache = self._evaluate_prompt(prompt_tokens)
+        mx.eval(logits) # So that we can more accurately reflect the prompt evaluation time.
+        prompt_time = time.time_ns() - start_time
+        yield {
+            "op": "evaluatedPrompt",
+            "prompt": prompt,
+            "token_count": len(prompt_tokens),
+            "time_ms": prompt_time / 1e6,
+            "prompt_tps": len(prompt_tokens) / (prompt_time / 1e9),
+        }
+
+        if schema:
+            token_acceptor = JsonSchemaAcceptorDriver(
+                schema,
+                self.vocabulary,
+                self.eos_id,
+                is_encapsulated_json=encapsulated,
+            )
+            if preemptive_batch_size > 0:
+                generator = self.generate_with_preemptive_decoding(
+                    logits,
+                    cache,
+                    token_acceptor,
+                    temp,
+                    max_batch_size=preemptive_batch_size,
+                )
+            else:
+                generator = self.generate_with_schema(
+                    logits, cache, token_acceptor, temp
+                )
+        else:
+            generator = self.generate_without_schema(logits, cache, temp)
+
+        token_count = 0
+        generation_time = 0
+        for generation_result in self._generate_tokens(generator, max_tokens):
+            if generation_result["op"] == "generatedTokens":
+                token_count += generation_result["token_count"]
+                generation_time += generation_result["time_ms"]
+            elif generation_result["op"] == "stop":
+                generation_result["token_count"] = token_count
+                generation_result["time_ms"] = generation_time
+                # This is slightly incorrect, because the first token is generated
+                # from the prompt evaluation.
+                generation_result["generation_tps"] = token_count / (
+                    generation_time / 1e3
+                )
+            yield generation_result
 
 
 def main():
@@ -447,9 +489,6 @@ def main():
         schema = None
     info("Starting generation...")
 
-    generated_token_count = 0
-    generation_time = 0.0
-
     for result in model.completion(
         prompt=args.prompt,
         schema=schema,
@@ -462,19 +501,18 @@ def main():
         if result["op"] == "evaluatedPrompt":
             prompt_token_count = result["token_count"]
             prompt_time = result["time_ms"]
+            prompt_tps = result["prompt_tps"]
             if args.repeat_prompt:
                 bolddim(result["prompt"], flush=True)
         elif result["op"] == "generatedTokens":
-            generated_token_count += result["token_count"]
-            generation_time += result["time_ms"]
             bold(result["text"], end="", flush=True)
         elif result["op"] == "stop":
             end_reason = result["reason"]
+            generated_token_count = result["token_count"]
+            generation_time = result["time_ms"]
+            generation_tps = result["generation_tps"]
         else:
             assert False
-
-    prompt_tps = prompt_token_count / (prompt_time / 1e3)
-    generation_tps = generated_token_count / (generation_time / 1e3)
 
     print()
     info(f"End reason: {end_reason}")
