6b7cddb7cbc4e577a9d874406c7d21cb464ff00e
Oscar Triscon <github@triscon.com>
2024-05-12 11:46:34 -0400

    Update to run with mlx-lm 0.13

--- src/examples/llm_schema.py
+++ src/examples/llm_schema.py
@@ -23,6 +23,8 @@ from llm_structured_output.util.bitmap import (
 from llm_structured_output.util.output import info, bold, bolddim, debug
 from llm_structured_output.util.tokenization import HuggingfaceTokenizerHelper
 
+from .reusable_kv_cache import ReusableKVCache
+
 
 class RejectedCompletion(Exception):
     """
@@ -62,37 +64,6 @@ class Model:
             )
         )
 
-    def _run_model(self, inputs: mx.array, cache=None):
-        """
-        This is like the model's __call__() method as implemented in
-        `mlx-examples/llms/mlx_lm/models/*.py`, except it is also able
-        to apply a causal mask once a cache is in place.
-        """
-        model = self.model.model
-        h = model.embed_tokens(inputs)
-
-        mask = None
-        T = h.shape[1]  # pylint: disable=invalid-name
-        if T > 1:
-            if cache is None:
-                S = 0  # pylint: disable=invalid-name
-            else:
-                S = cache[0][0].shape[2]  # pylint: disable=invalid-name
-            mask = nn.MultiHeadAttention.create_additive_causal_mask(S + T)
-            mask = mask.split([S])[1].astype(h.dtype)
-
-        if cache is None:
-            cache = [None] * len(model.layers)
-
-        for e, layer in enumerate(model.layers):
-            h, cache[e] = layer(h, mask, cache[e])
-
-        out = model.norm(h)
-        return self.model.lm_head(out), cache
-
-    def _decode(self, tokens):
-        return self.tokenizer.no_strip_decode(tokens)
-
     def _evaluate_prompt(
         self, prompt: list[int], prior_prompt: list[int] = None, prior_cache=None
     ):
@@ -103,20 +74,19 @@ class Model:
                 # save the past logits.
                 if i >= len(prompt) - 1 or prompt[i] != t:
                     break
-            cache = [
-                (
-                    layer_key_cache[:, :, :i, :],
-                    layer_value_cache[:, :, :i, :],
-                )
-                for layer_key_cache, layer_value_cache in prior_cache
-            ]
+            cache = prior_cache
+            for layer_cache in cache:
+                layer_cache.reuse(len(prompt), i)
             tokens = prompt[i:]
         else:
+            cache = ReusableKVCache.for_model(self.model)
             tokens = prompt
-            cache = None
 
-        logits, cache = self._run_model(mx.array(tokens)[None], cache)
+        logits = self.model(mx.array(tokens)[None], cache)
         return logits, cache
+
+    def _decode(self, tokens):
+        return self.tokenizer.no_strip_decode(tokens)
 
     def _debug_top_tokens(self, logits, count=10):
         token_logits = sorted(
@@ -164,7 +134,7 @@ class Model:
             yield tokens
             if tokens[-1] == self.eos_id:
                 break
-            logits, cache = self.model(mx.array(tokens)[None], cache)
+            logits = self.model(mx.array(tokens)[None], cache)
 
     def generate_with_schema(
         self, logits, cache, token_acceptor, temp: Optional[float] = 0.0
@@ -174,7 +144,7 @@ class Model:
             yield tokens
             if tokens[-1] == self.eos_id:
                 break
-            logits, cache = self.model(mx.array(tokens)[None], cache)
+            logits = self.model(mx.array(tokens)[None], cache)
 
     def generate_with_preemptive_decoding(
         self,
@@ -195,6 +165,7 @@ class Model:
         logits for the first token will be the same (as long as the model applies
         a causal mask), and we can sample those logits to select from which of the
         items in the batch we can select the second token.
+        In practice, this only seems to accelerate things for unquantized models.
         """
         # Sample token from prompt evaluation
         accepted_token_bitmap = token_acceptor.select_valid_tokens()
@@ -214,17 +185,13 @@ class Model:
                 for followup_token in enumerate_set_bits(accepted_token_bitmap):
                     batch.append([last_token, followup_token])
                 # Re-shape the cache to match the input.
-                cache = [
-                    (
-                        mx.concatenate([layer_key_cache] * len(batch)),
-                        mx.concatenate([layer_value_cache] * len(batch)),
-                    )
-                    for layer_key_cache, layer_value_cache in cache
-                ]
+                for layer_cache in cache:
+                    layer_cache.keys = mx.concatenate([layer_cache.keys] * len(batch))
+                    layer_cache.values = mx.concatenate([layer_cache.values] * len(batch))
             else:  # Otherwise, submit the normal one-token continuation.
                 batch = [[last_token]]
 
-            logits, cache = self._run_model(mx.array(batch), cache)
+            logits = self.model(mx.array(batch), cache)
             mx.eval(logits)
 
             first_token_logits = bias_logits(mx, logits[0, 0, :], accepted_token_bitmap)
@@ -257,102 +224,9 @@ class Model:
                 accepted_token_bitmap = token_acceptor.select_valid_tokens()
 
                 # Select the accepted generation in the cache, restoring it to batch dimension 1.
-                cache = [
-                    (
-                        layer_key_cache.split([index, index + 1])[1],
-                        layer_value_cache.split([index, index + 1])[1],
-                    )
-                    for layer_key_cache, layer_value_cache in cache
-                ]
-
-            yield tokens
-
-    def generate_with_preemptive_decoding_constant_batch(
-        self,
-        logits,
-        cache,
-        token_acceptor,
-        temp: Optional[float] = 0.0,
-        max_batch_size=5,
-    ):
-        """
-        [Experimental]
-        Same as generate_with_preemptive_decoding(), but keeping the batch size
-        constant through the generation to have consistent matrix sizes.
-        This is just for experimenting with performance.
-        """
-        # Sample token from prompt evaluation
-        accepted_token_bitmap = token_acceptor.select_valid_tokens()
-        first_token_logits = bias_logits(mx, logits[0, -1, :], accepted_token_bitmap)
-        first_token = self._sample(first_token_logits, temp)
-        tokens = [first_token]
-        yield tokens
-        token_acceptor.advance_token(first_token)
-        accepted_token_bitmap = token_acceptor.select_valid_tokens()
-
-        # Re-shape the cache to match the constant batch size.
-        cache = [
-            (
-                mx.array([layer_key_cache[0]] * max_batch_size),
-                mx.array([layer_value_cache[0]] * max_batch_size),
-            )
-            for layer_key_cache, layer_value_cache in cache
-        ]
-
-        while True:
-            last_token = tokens[-1]
-            if count_set_bits(accepted_token_bitmap) in range(1, max_batch_size + 1):
-                # If the number of possible follow-up tokens is small, submit for
-                # evaluation a batch of 2-token continuations.
-                batch = []
-                for followup_token in enumerate_set_bits(accepted_token_bitmap):
-                    batch.append([last_token, followup_token])
-            else:  # Otherwise, submit the normal one-token continuation.
-                batch = [[last_token]]
-
-            # Fill up to the batch capacity so that the matrices have the right shape.
-            batch_array = mx.array(batch + [batch[-1]] * (max_batch_size - len(batch)))
-
-            logits, cache = self._run_model(batch_array, cache)
-            mx.eval(logits)
-
-            first_token_logits = bias_logits(mx, logits[0, 0, :], accepted_token_bitmap)
-            first_token = self._sample(first_token_logits, temp)
-            tokens = [first_token]
-
-            if first_token == self.eos_id:
-                yield tokens
-                break
-
-            token_acceptor.advance_token(first_token)
-            accepted_token_bitmap = token_acceptor.select_valid_tokens()
-            if not accepted_token_bitmap:
-                raise RejectedCompletion()
-
-            # If we had submitted 2-token continuations, we can decode a second token
-            if len(batch[0]) > 1:
-                index = next(  # Find which of the second tokens was selected
-                    i
-                    for i, batch_item in enumerate(batch)
-                    if batch_item[1] == first_token
-                )
-                second_token_logits = bias_logits(
-                    mx, logits[index, 1, :], accepted_token_bitmap
-                )
-                second_token = self._sample(second_token_logits, temp)
-                tokens.append(second_token)
-
-                token_acceptor.advance_token(second_token)
-                accepted_token_bitmap = token_acceptor.select_valid_tokens()
-
-                # Select the accepted generation in the cache for the next round.
-                cache = [
-                    (
-                        mx.array([layer_key_cache[index]] * max_batch_size),
-                        mx.array([layer_value_cache[index]] * max_batch_size),
-                    )
-                    for layer_key_cache, layer_value_cache in cache
-                ]
+                for layer_cache in cache:
+                    layer_cache.keys = layer_cache.keys.split([index, index + 1])[1]
+                    layer_cache.values = layer_cache.values.split([index, index + 1])[1]
 
             yield tokens
 
---
+++ src/examples/reusable_kv_cache.py
@@ -0,0 +1,66 @@
+"""
+Helper with improvements over mlx-lm's KVCache.
+"""
+
+import mlx.core as mx
+from mlx_lm.models.base import KVCache
+
+
+class ReusableKVCache(KVCache):
+    """
+    Usability improvements over KVCache.
+    """
+
+    @classmethod
+    def for_model(cls, model):
+        kv_heads = (
+            [model.n_kv_heads] * len(model.layers)
+            if isinstance(model.n_kv_heads, int)
+            else model.n_kv_heads
+        )
+        return [cls(model.head_dim, n) for n in kv_heads]
+
+    def reuse(self, new_prompt_length, common_prefix_length):
+        """
+        Reuse (part of) this cache for a new prompt that shares a prefix with it.
+        """
+        if self.keys is None:
+            return
+        # Clip the cache to the common length.
+        self.offset = common_prefix_length
+        # Make sure the cache can fit the whole prompt. Because the offset is
+        # (very likely) not a multiple of the step size, update_and_fetch()
+        # won't resize the cache when evaluating the rest of the prompt as it
+        # would if it were an empty cache.
+        current_size = self.keys.shape[2]
+        if current_size < new_prompt_length:
+            n_steps = (self.step + new_prompt_length - 1) // self.step
+            add_shape = (1, self.n_kv_heads, n_steps * self.step - current_size, self.head_dim)
+            k_zeros = mx.zeros(add_shape, self.keys.dtype)
+            v_zeros = mx.zeros(add_shape, self.values.dtype)
+            self.keys = mx.concatenate([self.keys, k_zeros], axis=2)
+            self.values = mx.concatenate([self.values, v_zeros], axis=2)
+
+    def update_and_fetch(self, keys, values):
+        """
+        Override the base class method to allow the cache to be used with batches of
+        size greater than 1.
+        This is just a tiny change in the line that determines the shape.
+        """
+        prev = self.offset
+        if prev % self.step == 0:
+            n_steps = (self.step + keys.shape[2] - 1) // self.step
+            shape = (keys.shape[0], self.n_kv_heads, n_steps * self.step, self.head_dim)
+            new_k = mx.zeros(shape, keys.dtype)
+            new_v = mx.zeros(shape, values.dtype)
+            if self.keys is not None:
+                self.keys = mx.concatenate([self.keys, new_k], axis=2)
+                self.values = mx.concatenate([self.values, new_v], axis=2)
+            else:
+                self.keys, self.values = new_k, new_v
+    
+        self.offset += keys.shape[2]
+        self.keys[..., prev : self.offset, :] = keys
+        self.values[..., prev : self.offset, :] = values
+        return self.keys[..., : self.offset, :], self.values[..., : self.offset, :]
+
